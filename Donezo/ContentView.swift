3d9554b847a3672fb917b6f4f2453701367562f8
Abel <persistentabel@gmail.com>
2025-01-14 22:38:57 -0800

    [Clean] Updates to documentation

--- Donezo/ContentView.swift
+++ Donezo/ContentView.swift
@@ -5,6 +5,32 @@
 //  Created by Abel Gomez on 1/14/25.
 //
 
+/*
+ Clone = Copying the repo locally
+ Commit = Save ("checkpoint") on current branch
+ Stage = Prepare changes for a commit
+ Stash = Save changes for later
+ Push = Send local commits to remote repo
+ Pull = Fetch remote commits to local repo
+ 
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ RELEASE:
+ [Release] Description of release
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ */
+
 import SwiftUI
 
 struct ContentView: View {
