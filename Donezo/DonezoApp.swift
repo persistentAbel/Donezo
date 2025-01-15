7822a013d84fc042b466b1437b6badfca9d2ffa5
Abel <persistentabel@gmail.com>
2025-01-14 22:29:36 -0800

    Initial Commit

---
+++ Donezo/DonezoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonezoApp.swift
+//  Donezo
+//
+//  Created by Abel Gomez on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct DonezoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
